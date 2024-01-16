c9ff5aa7ad48d6dda85212aaa42df63a6f44c286
Tom Lorius <tomlorius.mail@gmail.com>
2024-01-16 11:39:02 +0100

    it 5-6 done

--- server.js
+++ server.js
@@ -1,21 +1,33 @@
-const express = require('express')
+const express = require("express");
+const projects = require("./data/projects.json");
+const articles = require("./data/articles.json");
 
-const app = express()
+const app = express();
+const logger = require("morgan");
+app.use(logger("dev"));
 
-app.use(express.json())
+app.use(express.json());
 
-app.get('/', (request, response) => {
-  response.send('<h1>Welcome Ironhacker. :) with nodemon</h1>')
-})
+app.get("/", (request, response) => {
+  response.send("<h1>Welcome Ironhacker. :) with nodemon</h1>");
+});
 
-app.get('/api/books', (_, response) => {
+app.get("/api/projects", (request, response) => {
+  response.json(projects);
+});
+
+app.get("/api/articles", (request, response) => {
+  response.json(articles);
+});
+
+/*app.get("/api/books", (_, response) => {
   const books = [
-    { title: 'Rick&Morty', pages: 35 },
-    { title: 'Solo Leveling', pages: 150 },
-    { title: 'Arslan', pages: 150 },
-  ]
+    { title: "Rick&Morty", pages: 35 },
+    { title: "Solo Leveling", pages: 150 },
+    { title: "Arslan", pages: 150 },
+  ];
 
-  response.json(books)
-})
+  response.json(books);
+});*/
 
-app.listen(3000, () => console.log('My first app listening on port 3000!'))
+app.listen(5005, () => console.log("My first app listening on port 5005!"));
